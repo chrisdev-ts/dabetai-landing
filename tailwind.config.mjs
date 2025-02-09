b8f619ed56e61f06c022fe66efe0c27ca898ac1e
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-02-09 12:08:23 -0600

    Estructuración y creación básica de componentes del proyecto según los mocups. Adición y configuración de TailwindCSS para manejar los estilos del proyecto. Adición de la fuente "Poppins". Desarrollo de las secciones "Header" y "Hero".

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0077B6",
+        secondary: "#DDF6FF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
