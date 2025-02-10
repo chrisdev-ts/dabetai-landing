2972b7e6c920c2516616734531eb3a2ddd5e6acd
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-02-09 19:10:57 -0600

    Implementación de la sección "How it works" enfocada a la aplicación móvil.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,7 @@ export default {
       colors: {
         primary: "#0077B6",
         secondary: "#DDF6FF",
+        tertiary: "#F1F5F9",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
